096709cc46744078ce40a44031e4de5fc0a9480d
zaeval <h3065@naver.com>
2017-03-28 15:33:25 +0900

    Initial Commit

---
+++ acm_icpc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  acm_icpc
+//
+//  Created by 홍승의 on 2017. 3. 28..
+//  Copyright © 2017년 홍승의. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
